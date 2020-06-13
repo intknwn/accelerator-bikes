78a91ea3a7f6554dc0c0dcd3a460c952a9374119
intknwn <intknwn@gmail.com>
2020-06-13 12:06:15 +0800

    Добавляет шапку и подвал

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,22 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+(function () {
+  var nav = document.querySelector('.site-nav');
+  var navToggle = document.querySelector('.site-header__nav-toggle');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+  nav.classList.remove('site-nav--nojs');
+  nav.classList.add('site-nav--closed');
+  navToggle.classList.add('site-header__nav-toggle--closed');
+
+  navToggle.addEventListener('click', function () {
+    if (nav.classList.contains('site-nav--closed')) {
+      nav.classList.remove('site-nav--closed');
+      nav.classList.add('site-nav--opened');
+      navToggle.classList.remove('site-header__nav-toggle--closed');
+    } else {
+      nav.classList.add('site-nav--closed');
+      nav.classList.remove('site-nav--opened');
+      navToggle.classList.add('site-header__nav-toggle--closed');
+    }
+  });
+})();
